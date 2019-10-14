d5ba394db033c5301e07534bcf39f6a5b350cfba
myyd <myyd9787@gmail.com>
2019-10-14 10:48:19 +0800

    zl

--- src/main/java/com/neuedu/mapper/RegisterMapper.java
+++ src/main/java/com/neuedu/mapper/RegisterMapper.java
@@ -1,27 +1,27 @@
-//package com.neuedu.mapper;
-//
-//import com.neuedu.pojo.Register;
-//
-//import java.util.List;
-//
-//public interface RegisterMapper {
-//
-//    int deleteByPrimaryKey(Integer ID);
-//
-//    int insert(Register record);
-//
-//    int insertSelective(Register record);
-//
-//    Register selectByPrimaryKey(Integer ID);
-//
-//    int updateByPrimaryKeySelective(Register record);
-//
-//    int updateByPrimaryKey(Register record);
-//
-//    /**
-//     * 根据挂号状态查询患者
-//     * @param state 状态码
-//     * @return
-//     */
-//    List<Register> getRegisterByState(Integer state);
-//}
+package com.neuedu.mapper;
+
+import com.neuedu.pojo.Register;
+
+import java.util.List;
+
+public interface RegisterMapper {
+
+    int deleteByPrimaryKey(Integer ID);
+
+    int insert(Register record);
+
+    int insertSelective(Register record);
+
+    Register selectByPrimaryKey(Integer ID);
+
+    int updateByPrimaryKeySelective(Register record);
+
+    int updateByPrimaryKey(Register record);
+
+    /**
+     * 根据挂号状态查询患者
+     * @param state 状态码
+     * @return
+     */
+    List<Register> getRegisterByState(Integer state);
+}
