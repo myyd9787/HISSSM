a35eca46fa8cf4f3afbbffd9965420b6cb9d227d
keep-snow <1799813043@qq.com>
2019-10-14 09:30:38 +0800

    zl

--- src/main/java/com/neuedu/service/impl/ManagerServiceImpl.java
+++ src/main/java/com/neuedu/service/impl/ManagerServiceImpl.java
@@ -1,12 +1,13 @@
-package com.neuedu.service;
+package com.neuedu.service.impl;
 
 import com.neuedu.mapper.UserMapper;
 import com.neuedu.pojo.User;
+import com.neuedu.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("managerService")
-public class ManagerServiceImpl implements ManagerService{
+public class ManagerServiceImpl implements ManagerService {
     @Autowired
     private UserMapper userMapper;
     //登录验证
